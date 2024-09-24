63c47c9a1d10bf0579b941f6c65f6756ed637c16
Бижова Андрей <andreybizhov@gmail.com>
2024-09-24 13:14:40 +0500

    задание

--- пупупу/пупупу.cpp
+++ пупупу/пупупу.cpp
@@ -5,43 +5,15 @@ int main()
 {
     setlocale(LC_ALL, "ru");
 
-    int num1, num2;
-    std::string z;
-
-    std::cout << "\t Калькулятор Made In RPO23-1\n";
-    std::cout << "\n";
-    std::cout << "Введите первое число: ";
-    std::cin >> num1;
-    std::cout << "Введите второе число: ";
-    std::cin >> num2;
-    std::cout << "Введите знак(+, -, *, /, %): ";
-    std::cin >> z;
-
-    if (z == "/" && num2 == 0)
-    {
-        std::cout << "На ноль делят только геи)";
-        return 1;
+    int num, total_sum = 0;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+    while (num != 0) {
+        total_sum += num;
+        std::cout << "Введите число: ";
+        std::cin >> num;
     }
-    if (z == "+")
-    {
-        std::cout << num1 << " + " << num2 << " = " << num1 + num2;
-    }
-    else if (z == "-")
-    {
-        std::cout << num1 << " - " << num2 << " = " << num1 - num2;
-    }
-    else if (z == "/")
-    {
-        std::cout << num1 << " / " << num2 << " = " << num1 / num2;
-    }
-    else if (z == "*")
-    {
-        std::cout << num1 << " * " << num2 << " = " << num1 * num2;
-    }
-    else if (z == "%")
-    {
-        std::cout << num1 << " % " << num2 << " = " << (num1 / 100) * num2;
-    }
-
+    std::cout << "Сумма равна " << total_sum << std::endl;
     return 0;
+
 }
