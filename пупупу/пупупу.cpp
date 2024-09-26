1ab2cb1227cf643fd820cfa7741d04df738633ce
Бижова Андрей <andreybizhov@gmail.com>
2024-09-26 15:34:56 +0500

    как то так

--- пупупу/пупупу.cpp
+++ пупупу/пупупу.cpp
@@ -1,99 +1,58 @@
 ﻿#include <iostream>
 
+int calculateFuel(int weight) {
+    if (weight <= 400) 
+    {
+        return 2;
+    }
+    else if (weight <= 900) 
+    {
+        return 5;
+    }
+    else if (weight <= 1400) 
+    {
+        return 8;
+    }
+    else if (weight <= 1900)
+    {
+        return 10;
+    }
+    else 
+    {
+        return -1;
+    }
+}
 
-int main()
+
+int main() 
 {
     setlocale(LC_ALL, "ru");
+    int weight, distanceAB, distanceBC;
 
+    std::cout << "Введите вес груза (в кг): ";
+    std::cin >> weight;
+    std::cout << "Введите расстояние от A до B (в км): ";
+    std::cin >> distanceAB;
+    std::cout << "Введите расстояние от B до C (в км): ";
+    std::cin >> distanceBC;
 
-    double user_summ;
-    int choise;
-    while (true)
-    {
-
-        std::cout << "\t Банк Made IN Фарит Приветствует вас! \n";
-        std::cout << "\n";
-        std::cout << "Введите сумму: ";
-        std::cin >> user_summ;
-        std::cout << "\n";
-        std::cout << "1) Евро \n";
-        std::cout << "2) Доллар \n";
-        std::cout << "3) Йена \n";
-        std::cout << "4) Фарит \n";
-        std::cout << "5) Юань \n";
-        std::cout << "\n";
-
-        std::cout << "В какую валюту хотите перевести?\n";
-        std::cin >> choise;
-
-        int com = (user_summ / 100) * 5;
-        int summ_with_com = user_summ - com;
-
-
-        if (choise == 1)
-        {
-            std::cout << "Коммиссия составила: " << com << "\n";
-            std::cout << "На ваш счет переведено: " << summ_with_com / 103.22 << " Евро";
-            std::cout << "\n";
-        }
-
-
-        else if (choise == 2)
-        {
-            std::cout << "Коммиссия составила: " << com << "\n";
-            std::cout << "На ваш счет переведено " << summ_with_com / 92.92 << " Долларов";
-            std::cout << "\n";
-            
-        }
-
-
-        else if (choise == 3)
-        {
-            std::cout << "Коммиссия составила: " << com << "\n";
-            std::cout << "На ваш счет переведено " << summ_with_com / 0.65 << " Йен";
-            std::cout << "\n";
-        }
-
-
-        else if (choise == 4)
-        {
-            std::cout << "Коммиссия составила: " << com << "\n";
-            std::cout << "На ваш счет переведено " << summ_with_com / 37 << " Фаритов";
-            std::cout << "\n";
-        }
-
-
-        else if (choise == 5)
-        {
-            std::cout << "Коммиссия составила: " << com << "\n";
-            std::cout << "На ваш счет переведено " << summ_with_com / 13.11 << " Юаней";
-            std::cout << "\n";
-        }
-
-
-        else
-        {
-            std::cout << "Неправильно!";
-        }
-
-
-        std::cout << "Желаете продолжить обмен валют? \n";
-        std::cout << "\n";
-        std::cout << "1 - да \n";
-        std::cout << "2 - Нет\n";
-        std::cout << "\n";
-        std::cout << "Введите ответ: ";
-        std::cin >> choise;
-
-        if (choise == 1)
-        {
-            continue;
-        }
-
-        else
-        {
-            break;
-        }
+    int fueluse = calculateFuel(weight);
+    if (fueluse == -1) {
+        std::cout << "Грузовик не едет с таким весом." << std::endl;
+        return 1;
     }
+    int totalFueluse = fueluse * (distanceAB + distanceBC);
+
+    int fueluseAB = fueluse * distanceAB;
+    int fuelToRefuel = totalFueluse - fueluseAB;
+
+    if (fuelToRefuel > 500) {
+        std::cout << "Не хватит топлива в баке." << std::endl;
+    }
+    else {
+        std::cout << "Необходимо заправить " << fuelToRefuel << " литров топлива в пункте B." << std::endl;
+    }
+
     return 0;
 }
+    
