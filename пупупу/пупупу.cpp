78252ceb6770708fc5b9f52ce1a2cf6c240c71f7
Бижова Андрей <andreybizhov@gmail.com>
2024-10-01 13:30:56 +0500

    ол

--- пупупу/пупупу.cpp
+++ пупупу/пупупу.cpp
@@ -1,58 +1,24 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-int calculateFuel(int weight) {
-    if (weight <= 400) 
-    {
-        return 2;
-    }
-    else if (weight <= 900) 
-    {
-        return 5;
-    }
-    else if (weight <= 1400) 
-    {
-        return 8;
-    }
-    else if (weight <= 1900)
-    {
-        return 10;
-    }
-    else 
-    {
-        return -1;
-    }
-}
-
-
-int main() 
-{
+int main() {
     setlocale(LC_ALL, "ru");
-    int weight, distanceAB, distanceBC;
+    srand(static_cast<unsigned int>(time(0)));
 
-    std::cout << "Введите вес груза (в кг): ";
-    std::cin >> weight;
-    std::cout << "Введите расстояние от A до B (в км): ";
-    std::cin >> distanceAB;
-    std::cout << "Введите расстояние от B до C (в км): ";
-    std::cin >> distanceBC;
+    int array[20];
 
-    int fueluse = calculateFuel(weight);
-    if (fueluse == -1) {
-        std::cout << "Грузовик не едет с таким весом." << std::endl;
-        return 1;
-    }
-    int totalFueluse = fueluse * (distanceAB + distanceBC);
-
-    int fueluseAB = fueluse * distanceAB;
-    int fuelToRefuel = totalFueluse - fueluseAB;
-
-    if (fuelToRefuel > 500) {
-        std::cout << "Не хватит топлива в баке." << std::endl;
-    }
-    else {
-        std::cout << "Необходимо заправить " << fuelToRefuel << " литров топлива в пункте B." << std::endl;
+    for (int i = 0; i < 20; i++) {
+        array[i] = rand() % 6 + 5;
     }
 
+    std::cout << "Числа в массиве:" << std::endl;
+    for (int i = 0; i < 20; i++) {
+        std::cout << array[i] << "\t";
+        if ((i + 1) % 10 == 0) {
+            std::cout << std::endl;
+        }
+    }
     return 0;
 }
-    
+
